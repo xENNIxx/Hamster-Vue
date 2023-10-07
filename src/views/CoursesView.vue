cbd9dbbe47b14e3ec5089e4f2fc13eee41646440
xENNIxx <enni2004@gmx.at>
2023-10-07 16:52:42 +0200

    ui for courses

--- src/views/CoursesView.vue
+++ src/views/CoursesView.vue
@@ -1,159 +1,44 @@
 <template>
-  <div class="collapse bg-base-200 collapse-arrow w-auto m-5 shadow-md">
-    <input type="checkbox" />
-    <div class="collapse-title text-xl font-medium">
-      1AHWII - SWP
-    </div>
-    <div class="collapse-content">
-      <table class="table">
-        <!-- head -->
-        <thead>
-          <tr>
-            <th>Lehrperson</th>
-            <th>Aufgabe</th>
-            <th>Abgabe</th>
-            <th></th>
-          </tr>
-        </thead>
-        <tbody>
-          <!-- row 1 -->
-          <tr>
-            <td>
-              <div class="font-bold">
-                Albert Greinöcker
-              </div>
-            </td>
-            <td>
-              If-Verzweigungen
-            </td>
-            <td>
-              25.11.2023
-            </td>
-            <th>
-              <button class="btn btn-success btn-xs">Löse</button>
-            </th>
-          </tr>
-
-          <!-- row 2 -->
-          <tr>
-            <td>
-              <div class="font-bold">
-                Albert Greinöcker
-              </div>
-            </td>
-            <td>
-              While-Schleifen
-            </td>
-            <td>
-              1.12.2023
-            </td>
-            <th>
-              <button class="btn btn-success btn-xs">Löse</button>
-            </th>
-          </tr>
-
-        </tbody>
-      </table>
-    </div>
-  </div>
-
-  <div class="collapse bg-base-200 collapse-arrow w-auto m-5 shadow-md">
-    <input type="checkbox" />
-    <div class="collapse-title text-xl font-medium">
-      5AHWII - SWP
-    </div>
-    <div class="collapse-content">
-      <table class="table">
-        <!-- head -->
-        <thead>
-          <tr>
-            <th>Lehrperson</th>
-            <th>Aufgabe</th>
-            <th>Abgabe</th>
-            <th></th>
-          </tr>
-        </thead>
-        <tbody>
-          <!-- row 1 -->
-          <tr>
-            <td>
-              <div class="font-bold">
-                Albert Greinöcker
-              </div>
-            </td>
-            <td>
-              If-Verzweigungen
-            </td>
-            <td>
-              25.11.2023
-            </td>
-            <th>
-              <button class="btn btn-success btn-xs">Löse</button>
-            </th>
-          </tr>
-
-          <!-- row 2 -->
-          <tr>
-            <td>
-              <div class="font-bold">
-                Albert Greinöcker
-              </div>
-            </td>
-            <td>
-              While-Schleifen
-            </td>
-            <td>
-              1.12.2023
-            </td>
-            <th>
-              <button class="btn btn-success btn-xs">Löse</button>
-            </th>
-          </tr>
-
-        </tbody>
-      </table>
-    </div>
-  </div>
-
-  <button @click="clickevent" class="btn btn-secondary">Get Courses</button>
-
-  <div v-for="course in get" class="collapse bg-base-200 collapse-arrow w-auto m-5 shadow-md">
-    <input type="checkbox" />
-    <div class="collapse-title text-xl font-medium">
-      {{course.name}}
-    </div>
-    <div class="collapse-content">
-      <table class="table">
-        <!-- head -->
-        <thead>
-          <tr>
-            <th>Lehrperson</th>
-            <th>Aufgabe</th>
-            <th>Abgabe</th>
-            <th></th>
-          </tr>
-        </thead>
-        <tbody>
-          <!-- row 1 -->
-          <tr>
-            <td>
-              <div class="font-bold">
+  <div class="flex flex-col items-center justify-center">
+    <button @click="clickevent" class="btn btn-secondary m-5">Get Courses</button>
+    <div v-for="course in get" class="collapse bg-base-200 collapse-arrow w-1/2 m-5 shadow-md">
+      <input type="checkbox" />
+      <div class="collapse-title text-xl font-medium">
+        {{ course.name }}
+      </div>
+      <div class="collapse-content">
+        <table class="table">
+          <!-- head -->
+          <thead>
+            <tr>
+              <th>Lehrperson</th>
+              <th>Aufgabe</th>
+              <th>Abgabe</th>
+              <th></th>
+            </tr>
+          </thead>
+          <tbody>
+            <!-- row 1 -->
+            <tr>
+              <td>
+                <div class="font-bold">
+                  must be added...
+                </div>
+              </td>
+              <td>
                 must be added...
-              </div>
-            </td>
-            <td>
-              must be added...
-            </td>
-            <td>
-              must be added...
-            </td>
-            <th>
-              <button class="btn btn-success btn-xs">Löse</button>
-            </th>
-          </tr>
+              </td>
+              <td>
+                must be added...
+              </td>
+              <th>
+                <button class="btn btn-success btn-xs">Löse</button>
+              </th>
+            </tr>
 
-        </tbody>
-      </table>
+          </tbody>
+        </table>
+      </div>
     </div>
   </div>
 </template>
@@ -178,7 +63,7 @@ export default {
         },
         withCredentials: true,
       };
-      
+
       try {
         const response = await axios(config);
         this.get = response.data;
