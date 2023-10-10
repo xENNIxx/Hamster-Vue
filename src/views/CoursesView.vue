fd941499387d40ea624cb756cde75309617c842e
xENNIxx <enni2004@gmx.at>
2023-10-10 21:21:56 +0200

    added details

--- src/views/CoursesView.vue
+++ src/views/CoursesView.vue
@@ -1,9 +1,11 @@
 <template>
-  <div class="flex flex-col items-center justify-center" >
-    <button @click="clickevent" @onload="clickevent" class="btn btn-ghost m-5">  
-      <svg class="w-6 h-6 text-primary" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 18">
-      <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m1 14 3-3m-3 3 3 3m-3-3h16v-3m2-7-3 3m3-3-3-3m3 3H3v3"/>
-    </svg></button>
+  <div class="flex flex-col items-center justify-center">
+    <button @click="clickevent" @onload="clickevent" class="btn btn-ghost m-5">
+      <svg class="w-6 h-6 text-primary" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none"
+        viewBox="0 0 20 18">
+        <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+          d="m1 14 3-3m-3 3 3 3m-3-3h16v-3m2-7-3 3m3-3-3-3m3 3H3v3" />
+      </svg></button>
     <div v-for="course in get" class="collapse bg-base-200 collapse-arrow w-1/2 m-5 shadow-md">
       <input type="checkbox" />
       <div class="collapse-title text-xl font-medium">
@@ -28,10 +30,21 @@
                 </div>
               </td>
               <td>
-                must be added...
+                01.01.0001
               </td>
               <th>
-                <button class="btn btn-secondary btn-xs">Details</button>
+              <!-- ignore checkbox -->
+              <input type="checkbox" :id="exercise.exercise_name" class="modal-toggle" />
+                <div class="modal">
+                  <div class="modal-box">
+                    <h3 class="font-bold text-lg">Details</h3>
+                    <p class="py-4">{{exercise.exercise_name}}</p>
+                    <div class="modal-action">
+                      <label :for="exercise.exercise_name" class="btn">Close</label>
+                    </div>
+                  </div>
+                </div>
+                <label class="btn btn-secondary btn-xs m-5" :for="exercise.exercise_name" >Details</label>
                 <button class="btn btn-success btn-xs">Löse</button>
               </th>
             </tr>
@@ -40,7 +53,6 @@
         </table>
       </div>
     </div>
-
   </div>
 </template>
 
@@ -76,7 +88,7 @@ export default {
     }
   },
   beforeMount() {
-   this.clickevent()
+    this.clickevent()
   },
 }
 </script>
